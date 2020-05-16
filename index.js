c9a223918dbcbdab4892616ba90a0ab819486370
swapman972 <jordan.melidor@hotmail.fr>
2020-05-16 17:19:59 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+    let answer = array.find( (w) => {
+      if (w.result === "W"){
+        return w
+      } 
+    })
+    if (answer){
+      return answer.year
+    } else return answer
+}
